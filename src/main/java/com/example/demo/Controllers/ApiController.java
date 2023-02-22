8fc56ac46069c8c47c7442a74ba765181054ef3e
KAMALESH481 <727721euit066@skcet.ac.in>
2023-02-22 10:25:01 +0530

    First commit

---
+++ src/main/java/com/example/demo/Controllers/ApiController.java
@@ -0,0 +1,18 @@
+package com.example.demo.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+	String str="Kamalesh";
+	
+	@GetMapping("/")
+	public String getName()
+	{
+		return "Welcome "+str+" !";
+	}
+	 
+}
+
+
